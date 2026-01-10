8483a52e6a5786133fbc1a9521202a68600cbbca
rafael456103 <rafaelvargas983y@gmail.com>
2026-01-10 17:24:39 -0600

    self authenticacion provider

--- src/main/java/com/project/demo/securityConfig/CustomAuthProvider.java
+++ src/main/java/com/project/demo/securityConfig/CustomAuthProvider.java
@@ -11,7 +11,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -23,12 +23,35 @@ import java.util.Optional;
 public class CustomAuthProvider implements AuthenticationProvider {
 
     @Autowired
-    StudentRepository studentRepository;
+    BCryptPasswordEncoder passwordEncoder;
     @Autowired
-    PasswordEncoder passwordEncoder;
+    StudentRepository studentRepository;
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        return null;
+        String email = authentication.getName();
+        String rawpassword = authentication.getCredentials().toString();
+
+        //find email student
+        Optional<Student> studentOptional = studentRepository.findByEmail(email);
+        if(studentOptional.isEmpty()){
+            log.warn("Email student dont founded");
+            new BadCredentialsException("Email student dont founded");
+        }
+
+        Student student = studentOptional.get();
+        if(passwordEncoder.matches(rawpassword, student.getPassword())){
+            log.warn("Password does not match");
+            new BadCredentialsException("Password does not match");
+        }
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_STUDENT"));
+
+        return UsernamePasswordAuthenticationToken.authenticated(email, null,authorities);
+    }
+    @Override
+    public boolean supports(Class<?> authenticacion){
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authenticacion);
     }
 }
