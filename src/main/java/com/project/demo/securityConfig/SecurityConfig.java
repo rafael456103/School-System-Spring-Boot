a9026cc32c299225351020edbf21bc5791fc5454
rafael456103 <rafaelvargas983y@gmail.com>
2026-04-02 20:11:21 -0600

    The settings center was changed

--- src/main/java/com/project/demo/securityConfig/SecurityConfig.java
+++ src/main/java/com/project/demo/securityConfig/SecurityConfig.java
@@ -1,41 +1,34 @@
 package com.project.demo.securityConfig;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableMethodSecurity
 @Configuration
 public class SecurityConfig{
 
-    @Autowired
-    AuthenticationProvider authenticationProvider;
+    private final AuthenticationProvider authenticationProvider;
+    public SecurityConfig(AuthenticationProvider authenticationProvider){
+        this.authenticationProvider = authenticationProvider;
+    }
 
-    //Simple Security Filter Chain
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(
-                                "/api/SchoolSystem/login",
-                                "/api/SchoolSystem/register")
-                        .permitAll().anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize ->{authorize
+                        .requestMatchers("/api/SchoolSystem/login","/api/SchoolSystem/register")
+                        .permitAll().anyRequest().permitAll();
+                })
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider);
         return httpSecurity.build();
-    }
-
-    //Password encoder with BCrypt
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
     }
 }
