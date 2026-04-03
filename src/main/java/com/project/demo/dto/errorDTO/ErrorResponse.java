d1215c66676677ead82a0c29e0785743d8342cc0
rafael456103 <rafaelvargas983y@gmail.com>
2026-04-02 20:11:49 -0600

    Added error response entity

--- src/main/java/com/project/demo/dto/errorDTO/ErrorResponse.java
+++ src/main/java/com/project/demo/dto/errorDTO/ErrorResponse.java
@@ -1,5 +1,19 @@
 package com.project.demo.dto.errorDTO;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ErrorResponse {
 
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
 }
