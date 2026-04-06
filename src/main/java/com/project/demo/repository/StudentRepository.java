3d7fedf45eb2025c3b57e74c459cee63960554d6
rafael456103 <rafaelvargas983y@gmail.com>
2026-04-05 18:56:06 -0600

    added method for students

--- src/main/java/com/project/demo/repository/StudentRepository.java
+++ src/main/java/com/project/demo/repository/StudentRepository.java
@@ -1,11 +1,17 @@
 package com.project.demo.repository;
 
+import com.project.demo.dto.studentDTO.StudentResponseDTO;
 import com.project.demo.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
+    @Query("SELECT new com.project.demo.dto.studentDTO.StudentResponseDTO(s.id, s.name, s.university, s.course, s.email) FROM Student s")
+    List<StudentResponseDTO> getStudents();
     Optional<Student> findByEmail(String email);
+
 }
